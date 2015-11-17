c2bd73b152e60a2b09291404ce99db911e8a4070
ChristiansenL <lasse.christiansen@nordakademie.de>
2015-11-17 12:01:04 +0100

    Summe bis Maximum

--- src/uebung_4_0/SummeBisMaximum.java
+++ src/uebung_4_0/SummeBisMaximum.java
@@ -3,13 +3,14 @@ package uebung_4_0;
 public class SummeBisMaximum {
 
 	public static void main(String[] args) {
-		int maximum = 1000;
+		int maximum = 50000;
 		int summe = 0;
-		
-		for (int x = 1; x < maximum; x++) {
+
+		for (int x = 1; x <= maximum; x++) {
 			summe = summe + x;
 			if (summe < maximum) {
-			System.out.println(summe);}
+				System.out.println(summe);
+			}
 		}
 
 	}
